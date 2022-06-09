3d96495a29511c3eb74a398810cee317dc0c089d
Albert <47365826+os3albert@users.noreply.github.com>
2022-06-09 14:29:31 +0200

    Updated licenser.java

--- src/main/java/com/acme/license/Licenser.java
+++ src/main/java/com/acme/license/Licenser.java
@@ -4,14 +4,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Builder
 public class Licenser {
 	
 	private List<Licensable> licensables = new ArrayList<Licensable>(); // altro commento
